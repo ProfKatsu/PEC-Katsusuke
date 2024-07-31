8e2bf7f4997f9cb30e0d8b309723e1047f3ddcc9
ProfKatsu <150474454+ProfKatsu@users.noreply.github.com>
2024-07-31 08:48:38 -0300

    Atualizar 'main.blocks, main.ts'

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Heart)
+    basic.showIcon(IconNames.Sad)
+    music.play(music.stringPlayable("A D G F D G E C ", 384), music.PlaybackMode.UntilDone)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
 })
 basic.forever(function () {
 	
